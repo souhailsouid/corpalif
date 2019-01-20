eec07f99da3ef3b812565abd43b7e07773b887a2
Souhail <mr.souid@live.fr>
2019-01-20 12:52:44 +0100

    upload

--- server/server.js
+++ server/server.js
@@ -464,9 +464,10 @@ app.post('/api/charge50', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
 	//Set static folder
 }
+
 app.use(express.static('client/build'))
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html', 'uploads'))
 })
 const port = process.env.PORT || 5000
 
